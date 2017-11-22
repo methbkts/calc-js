97e70c01208e2697c7a299350798d258efc8441b
Meth <30674934+meth-bkts@users.noreply.github.com>
2017-11-23 00:06:34 +0100

    correc title

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-var myCalc
+var inputLabel = document.getElementById('inputLabel');
+
+function pushBtn(obj) {
+
+    var pushed = obj.innerHTML;
+
+    if (pushed == '=') {
+            // Calculate
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+
+        } else if (pushed == 'AC') {
+            // All Clear
+            inputLabel.innerHTML = '0';
+
+        } else {
+            if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+
+            } else {
+                inputLabel.innerHTML += pushed;
+
+            }
+        }
+    }
+    
